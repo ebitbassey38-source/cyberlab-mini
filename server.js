da3199dcbad616a60ac5cee1b217251bf1ee53e6
ebitbassey38-source <ebitbassey38@gmail.com>
2026-07-19 09:35:07 +0100

    Fix helmet CSP blocking fetch

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
